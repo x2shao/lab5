36e0b2cd8b14f7acb497ad62e82da2a83c91ced5
Xinyu Shao <x2shao@ucsd.edu>
2018-02-08 10:12:00 -0800

    commit message

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
+
 	console.log(data);
-	res.render('index');
+	res.render('index'{
+		'name': 'Fake Friend',
+		'image':'http://lorempixel.com/500/500/people'
+
+	});
 };
